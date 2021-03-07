4b9996e1a9a4f46ec3e45d6377f565732985f82d
pipesoto13 <51277503+pipesoto13@users.noreply.github.com>
2021-03-07 11:56:21 -0500

    Create navbar component and its styles (#5)
    * Create navbar component and its styles
    
    * Use icon images from fontawesome
    
    * Update styles
    
    * Edit style

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { users } from "../../data";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons'
+import './style.css';
+
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <div className="nav__container">
+                <div className="nav__icon">
+                    <FontAwesomeIcon icon={faMotorcycle}/>
+                </div>
+                <ul className="nav__list">
+                    <li className="nav__items">
+                        <a href="#top">Historial</a>
+                    </li>
+                    <li className="nav__items">
+                        <a href="#top">Notificaciones</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="nav__profile">
+                <span>{users[1].name}</span>
+                <a href="#top">
+                    <img className="nav__userPhoto" src={users[1].photo} alt="profile_photo"/>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
