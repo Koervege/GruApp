b47a80cb9e1ba755d4b1f0f7a797e60779bdb4bd
Alexis Franco <alexisfrancolopez@gmail.com>
2021-03-21 18:23:18 -0500

    Axios for listTow

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -1,38 +1,22 @@
 import { Photo, ContainerList, ContainerElement, SectionList } from './styles';
 
-function Search({ users, initLoc, finalLoc, date, motoid, motorcycles }) {
-  const [moto] = motorcycles.filter((motorcycle) => motoid === motorcycle._id);
-
-  const [user] = users.filter((user) => moto.userID === user._id);
-
-  return (
-    <ContainerList>
-      <ContainerElement>{user.name}</ContainerElement>
-      <ContainerElement>{`${initLoc} / ${finalLoc} / ${date} / ${moto.type} / ${moto.cc}`}</ContainerElement>
-      <ContainerElement>
-        <Photo src={user.photo} alt={user.name}></Photo>
-      </ContainerElement>
-    </ContainerList>
-  );
-}
-
-function Provider({ services, motorcycles, users }) {
+function Provider({ services }) {
   return (
     <SectionList>
       {!!services &&
         services.length > 0 &&
-        services.map(({ _id, initLoc, finalLoc, date, motoID }) => {
+        services.map(({ _id, initLoc, finalLoc, date, bikeID }) => {
           return (
-            <Search
-              key={_id}
-              users={users}
-              services={services}
-              motoid={motoID}
-              motorcycles={motorcycles}
-              initLoc={initLoc}
-              finalLoc={finalLoc}
-              date={date}
-            />
+            <ContainerList key={_id}>
+              <ContainerElement>{bikeID.userId.name}</ContainerElement>
+              <ContainerElement>{`${initLoc} / ${finalLoc} / ${date} / ${bikeID.type} / ${bikeID.cc} cc`}</ContainerElement>
+              <ContainerElement>
+                <Photo
+                  src={bikeID.userId.photo}
+                  alt={bikeID.userId.name}
+                ></Photo>
+              </ContainerElement>
+            </ContainerList>
           );
         })}
     </SectionList>
