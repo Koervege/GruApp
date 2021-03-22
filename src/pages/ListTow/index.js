07e9f171605d54d10d0d99b7746dd842692daeae
AlexisFranco <alexisfrancolopez@gmail.com>
2021-03-22 11:39:30 -0500

    Axios for listTow

--- src/pages/ListTow/index.js
+++ src/pages/ListTow/index.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { services, motorcycles, users } from '../../data';
+import axios from 'axios';
 import { BoxSupplier } from '../ListMotorcycle/styles';
 import Provider from '../../components/Provider';
 import Button from '../../components/Button';
 import NavBar from '../../components/NavBar';
 class ListTow extends React.Component {
   state = {
-    services,
-    motorcycles,
-    users,
+    services: '',
+    error: '',
   };
 
+  async componentDidMount() {
+    try {
+      const { data: { services } } = await axios({
+        method: 'GET',
+        baseURL: process.env.REACT_APP_SERVER_URL,
+        url: '/services',
+      });
+
+      this.setState({
+        services,
+      });
+    } catch (error) {
+      this.setState({
+        error,
+      });
+      alert(error);
+    }
+  }
   render() {
-    const { services, motorcycles, users } = this.state;
+    const { services } = this.state;
     return (
       <section>
         <NavBar userId={this.props.match.params.id} />
         <BoxSupplier>
           <Button color="primary">Ha ganado XX.XXX COP</Button>
-          <Provider
-            services={services}
-            motorcycles={motorcycles}
-            users={users}
-          />
+          <Provider services={services} />
           <Button color="success">Servicio en proceso</Button>
         </BoxSupplier>
       </section>
