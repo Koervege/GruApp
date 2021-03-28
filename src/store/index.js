eee61581fa12aed2fd37f6c5e2e1e413f8111c92
Alexis Franco <alexisfrancolopez@gmail.com>
2021-03-28 07:18:08 -0500

    Redux to clients

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { servicesReducer } from './servicesReducer';
+import { clientsReducer } from './clientsReducer';
+import { suppliersReducer } from './suppliersReducer';
+
 
 const rootReducer = combineReducers({
   servicesReducer,
+  clientsReducer,
+  suppliersReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
