237c09514e885e30fd0484945e4877dace1fdaa1
Fernado Javier España Cordoba <49051793+FernandoEspana@users.noreply.github.com>
2021-04-05 16:02:53 -0500

    Hooks was implemented  (#28)
    * The class component ws changed to function component using hooks, useForm hook was implemented to manage login from inputs and handleChange event
    
    * Erase log issues
    
    * Identation fix
    
    * Identation fix and use-state import

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,6 +8,9 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios'; 
 import {ATags} from '../../components/NavBar/styles'
+import { useForm } from '../../hooks/useForm';
+import { useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 const StyledLink = styled(Link)`
 	text-decoration: none;
@@ -31,85 +34,73 @@ const StyledLink = styled(Link)`
     color: black;
   }
 `
-class Login extends React.Component {
-  state = {
+function Login() {
+
+  const [formValues, handleInputChange] = useForm({
     email: '',
     password: '',
-    error: '',
-  };
+  });
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
+const [error, setError] = useState('');
+const { email, password } = formValues;
+let history = useHistory();
 
-  searchUser = async (event) => {
-    event.preventDefault();
-
+const searchUser = async (event) => {
+  event.preventDefault();
     try {
-      const { data: { token, userType } }= await axios({
+      const { data: { token, userType,userFront } }= await axios({
         method: 'POST',
         baseURL: process.env.REACT_APP_SERVER_URL,
         url: '/users/signin',
-        data: this.state
+        data:formValues
       });
-      
       localStorage.setItem('token', token);
-      userType === 'client' ? 
-        this.props.history.push('/listmotorcycle') 
+      (userType === 'client')?
+        history.push('/listmotorcycle')
         :
-        this.props.history.push('/listtow'); 
+        history.push('/listtow'); 
     } catch (err) {
-        this.setState({
-          error: err,
-        });
+        setError(err);
         alert('Usuario o contraseña equivocados');
     }
   };
 
-  render() {
-    const { email, password } = this.state;
-
-    return (
-      <Frame>
+  return (
+    <Frame>
+      <Container>
+        <Img src={logo} radius="100" width="100" height="100" alt="logo" />
+      </Container>
+      <form onSubmit={ searchUser }>
+        <StyledInput
+          value={ email }
+          name="email"
+          onChange={ handleInputChange }
+          children="Email"
+          type="email"
+          required="required"
+        />
+        <StyledInput
+          name="password"
+          children="Pass"
+          value={ password }
+          onChange={ handleInputChange }
+          type="password"
+          required="required"
+        />
         <Container>
-          <Img src={logo} radius="100" width="100" height="100" alt="logo" />
+          <Button type="submit" color="primary">
+            Login
+          </Button>
+          <StyledLink to="/">Cancelar</StyledLink>
         </Container>
-        <form onSubmit={this.searchUser}>
-          <StyledInput
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-            children="Email"
-            type="email"
-            required="required"
-          />
-          <StyledInput
-            name="password"
-            children="Pass"
-            value={password}
-            onChange={this.handleChange}
-            type="password"
-            required="required"
-          />
-          <Container>
-            <Button type="submit" color="primary">
-              Login
-            </Button>
-            <StyledLink to="/">Cancelar</StyledLink>
-          </Container>
-        </form>
-
-        <Container>
-          <small>
-            Aun no estás registrado? <ATags to="/register">Registrarse</ATags>
-          </small>
-        </Container>
-      </Frame>
+      </form>
+      <Container>
+        <small>
+          Aun no estás registrado? <ATags to="/register">Registrarse</ATags>
+        </small>
+      </Container>
+    </Frame>
     );
-  }
-}
+}  
 
 export default Login;
