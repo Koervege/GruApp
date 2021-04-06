f6980fbc44f4c881e0ce6c783ddbc25c71929f1c
Felipe Soto Correa <pipesoto13@gmail.com>
2021-04-06 17:24:48 -0500

    Can upload images, add sweet alert, improve page behavior

--- src/pages/UserInfo/index.js
+++ src/pages/UserInfo/index.js
@@ -6,10 +6,12 @@ import Img from '../../components/Img';
 import MotoInfo from '../../components/MotoInfo';
 import TowInfo from '../../components/TowInfo';
 import { StyledInput, Container } from '../../components/StyledInput/index';
-import { StyledLink, StyledFieldset } from "./styles";
+import { StyledLink, StyledFieldset,ImgUser } from "./styles";
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import swal from 'sweetalert';
+
 
 
 function UserInfo() {
@@ -31,18 +33,31 @@ function UserInfo() {
       plateNum:'',
       type:'',
       weight:'',
-      photo: '',
-      vehiPhoto: '',
+      photo: null,
+      vehiPhoto: null,
+      image: null,
       vehicleType:'',
-      edit: false,
-      editVehi: false,
-      hideInfo: true,
+      editUser: false,
+      editVehi: true,
       error: null,
     }
   );
 
+  function readFile(file) {
+    const reader = new FileReader()
+
+    reader.readAsDataURL(file)
+
+    reader.onload = e => setState(prevState =>({ ...prevState, image: e.target.result}))
+    reader.onerror = e => console.log(reader.error)
+  }
+
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    let { name, value } = event.target;
+    if (name === 'photo' || name === 'vehiPhoto') {
+      value = event.target.files[0]
+      readFile(event.target.files[0])
+    }
     setState(prevState => ({
       ...prevState,
       [name]: value,
@@ -52,8 +67,8 @@ function UserInfo() {
   const handleEdit = (e) => {
     setState(prevState =>({
       ...prevState,
-      edit: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
-      editVehi: !state.editVehi,
+      editUser: e.target.name === 'editUser' ? !editUser : editUser,
+      editVehi: e.target.name === 'editVehi' ? !editVehi : editVehi,
     }));
   }
 
@@ -106,84 +121,109 @@ function UserInfo() {
         Authorization: `Bearer ${token}`,
       } 
     })
+    swal("¡Gracias por visitarnos!", "Usuario eliminado satisfacctoriamente", "info")
     history.push(`/login/`);
     localStorage.clear();
   }; 
 
-  const sendInfo = async(event) => {
+  const updateUser = async(event) => {
     event.preventDefault();
     const token = localStorage.getItem('token');
     const dataUser = new FormData();
     dataUser.append('name', state.name);
     dataUser.append('phoneNum', state.phoneNum);
-    if (state.edit) {
-      const { data } = await axios({
-        method: 'PUT',
-        baseURL:process.env.REACT_APP_SERVER_URL,
-        url: `/${userType}s`,
-        data: dataUser,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'multipart/form-data',
-        } 
-      })
-      alert(data.message)
-      const dataMoto = new FormData();
-      dataMoto.append('brand', state.brand);
-      dataMoto.append('cc', state.cc);
-      dataMoto.append('type', state.type);
-      dataMoto.append('plateNum', state.plateNum);
-      dataMoto.append('weight', state.weight);
-      const dataTow = new FormData();
-      dataTow.append('brand', state.brand);
-      dataTow.append('capacity', state.capacity);
-      dataTow.append('plateNum', state.plateNum);
-      dataTow.append('status', true);
-      await axios({
-        method : 'PUT',
-        baseURL:process.env.REACT_APP_SERVER_URL,
-        url: userType === 'client' ? '/motorcycles' : '/tows',
-        data: userType === 'client' ? dataMoto : dataTow,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'multipart/form-data',
-        } 
-      })
-    } else {
-      await axios({
-        method: 'POST',
-        baseURL:process.env.REACT_APP_SERVER_URL,
-        url: userType === 'client' ? '/motorcycles' : '/tows',
-        data: userType === 'client' ? 
-          {
-            brand: state.brand,
-            cc: state.cc,
-            type: state.type,
-            plateNum: state.plateNum,
-            weight: state.weight,
-          }
-          : 
-          {
-            brand: state.brand,
-            capacity: state.capacity,
-            plateNum: state.plateNum,
-            status: true,
-          },
-        headers: {
-          Authorization: `Bearer ${token}`,
-        } 
-      })
-      alert('vehiculo creador')
-
-      if(userType === 'client') {
-        history.push('/listMotorcycle/')
-      } else {
-        history.push('/listTow/')
-      }
+    console.log(state.photo);
+    if(state.photo) {
+      dataUser.append('photo', state.photo, state.photo.name)
     }
+    
+    const { data } = await axios({
+      method: 'PUT',
+      baseURL:process.env.REACT_APP_SERVER_URL,
+      url: `/${userType}s`,
+      data: dataUser,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      } 
+    })
+    swal("¡Buen trabajo!", "Usuario actualizado satisfacctoriamente", "success")
   }; 
 
-    const { name, phoneNum, brand, cc, capacity, plateNum, type, weight, photo, vehiPhoto, vehicleType, edit, editVehi } = state;
+  const updateVehi = async(event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+    const dataMoto = new FormData();
+    dataMoto.append('brand', state.brand);
+    dataMoto.append('cc', state.cc);
+    dataMoto.append('type', state.type);
+    dataMoto.append('plateNum', state.plateNum);
+    dataMoto.append('weight', state.weight);
+    const dataTow = new FormData();
+    dataTow.append('brand', state.brand);
+    dataTow.append('capacity', state.capacity);
+    dataTow.append('plateNum', state.plateNum);
+    dataTow.append('status', true);
+
+    await axios({
+      method : 'PUT',
+      baseURL:process.env.REACT_APP_SERVER_URL,
+      url: userType === 'client' ? '/motorcycles' : '/tows',
+      data: userType === 'client' ? dataMoto : dataTow,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      } 
+    })
+    .then(() => {
+      swal("¡Buen trabajo!", `${vehicleType} actualizada con éxito`, "success")
+    })
+    .catch(err => {
+      swal("¡No fué posible!", 'Debes tener un vehículo agregado', "error")
+    })
+  };
+
+  const createVehi = async(event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+
+    await axios({
+      method: 'POST',
+      baseURL:process.env.REACT_APP_SERVER_URL,
+      url: userType === 'client' ? '/motorcycles' : '/tows',
+      data: userType === 'client' ? 
+        {
+          brand: state.brand,
+          cc: state.cc,
+          type: state.type,
+          plateNum: state.plateNum,
+          weight: state.weight,
+        }
+        : 
+        {
+          brand: state.brand,
+          capacity: state.capacity,
+          plateNum: state.plateNum,
+          status: true,
+        },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      } 
+    })
+    swal("¡Buen trabajo!", `${vehicleType} creada con éxito`, "success")
+  }; 
+  
+  const accept = (event) => {
+    event.preventDefault();
+
+    if(userType === 'client') {
+      history.push('/listMotorcycle/')
+    } else {
+      history.push('/listTow/')
+    }
+  };
+
+    const { name, phoneNum, brand, cc, capacity, plateNum, type, weight, image, vehiPhoto, vehicleType, editUser, editVehi } = state;
 
     return (
       <Frame>
@@ -191,49 +231,52 @@ function UserInfo() {
           <Img src={logo} radius="100" width="100" height="100" alt="logo" />
         </Container>
 
-        <form onSubmit={sendInfo}>
-        <StyledInput
-            name="edit"
-            onChange={handleEdit}
-            children="Editar"
-            type="checkbox"
-            checked={edit}
-          />
+        <form onSubmit={createVehi}> 
         <StyledFieldset>
           <legend>Usuario</legend>
+          <label htmlFor="editUser">
+            <input type="checkbox" defaultChecked="true" id="editUser" name="editUser" onChange={handleEdit} checked={editUser}/>
+            Editar
+          </label>
           <StyledInput
             value={name}
             name="name"
             onChange={handleChange}
             children="Nombre"
             type="text"
-            disabled={!edit}
+            disabled={!editUser}
           />
-          <Button type="button" color="danger" onClick={eraseUser}>Borrar Usuario</Button>
           <StyledInput
             value={phoneNum}
             name="phoneNum"
             onChange={handleChange}
             children="Telefono"
             type="tel"
-            disabled={!edit}
+            disabled={!editUser}
           />
           <StyledInput
-            value={photo}
+            //value={photo}
             name="photo"
             onChange={handleChange}
             children="Foto perfil"
-            type="text"
-            disabled={!edit}
+            type="file"
+            accept="image/*"
+            id="photo"
+            disabled={!editUser}
           />
+          {image && <ImgUser src={image} width="75" height="75" alt="profile preview" />}
+          <Container>
+            <Button type="button" color="danger" onClick={eraseUser}>Borrar Usuario</Button>
+            {editUser && <Button type="submit" color="success" onClick={updateUser}>Actualizar Usuario</Button>}
+          </Container>
         </StyledFieldset>
-        { !plateNum && 
-        <Button type='button' color='success' onClick={() => setState(prevState =>({...prevState, hideInfo:false, editVehi:true}))}>
-          {vehicleType === 'Moto'? 'Agregar Moto': 'Agregar Grúa'}
-        </Button>
-        }
+      
         {(<StyledFieldset>
-          <legend>{vehicleType}</legend>
+          <legend>{vehicleType}</legend>  
+          <label htmlFor="editVehi">
+            <input type="checkbox" defaultChecked="true" id="editVehi" name="editVehi" onChange={handleEdit} checked={editVehi}/>
+            Editar
+          </label>
           {
             vehicleType === 'Moto'? 
               (
@@ -262,17 +305,20 @@ function UserInfo() {
             value={vehiPhoto}
             name="vehiPhoto"
             onChange={handleChange}
-            children="Foto vehículo "
-            type="text"
+            children="Foto vehículo"
+            type="file"
+            accept="image/*"
             disabled={!editVehi}
-          />  
-        </StyledFieldset>)}
+          />
           <Container>
-            <Button type="submit" color="primary">
-              Aceptar
-            </Button>
-            <StyledLink to="/">Cancelar</StyledLink>
+            <Button type="submit" color="primary" >Agregar {vehicleType}</Button>
+          { editVehi && plateNum && <Button type="submit" color="success" onClick={updateVehi}>Actualizar {vehicleType}</Button>}
           </Container>
+        </StyledFieldset>)}
+        <Container>
+          <Button type="submit" color="primary" onClick={accept} >Aceptar</Button>
+          <StyledLink to="/">Cancelar</StyledLink>
+        </Container>
         </form>
       </Frame>
     );
