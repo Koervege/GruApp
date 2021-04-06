d3fbb0aee9b2eee5499023c55b2828c1874405b5
Alexis Franco <alexisfrancolopez@gmail.com>
2021-04-05 21:40:50 -0500

    Use getLoggedUser from usersReducer to get information of user logged

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { towsReducer } from './towsReducer';
-import { clientsReducer } from './clientsReducer';
 import { servicesReducer } from './servicesReducer';
-import { suppliersReducer } from './suppliersReducer';
 import { usersReducer } from './usersReducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 
 const rootReducer = combineReducers({
   towsReducer,
-  clientsReducer,
   servicesReducer,
-  suppliersReducer,
   usersReducer
 });
 
