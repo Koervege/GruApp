a1a3fdeb20cf9b2bfec1d174d6da4fe23cb0c697
Alexis Franco <alexisfrancolopez@gmail.com>
2021-04-06 04:47:17 -0500

    Initial work in service creation

--- src/components/Client/index.js
+++ src/components/Client/index.js
@@ -1,17 +1,68 @@
+import swal from '@sweetalert/with-react';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
 import {
   Photo,
   ContainerList,
   ContainerElement,
   SectionList,
 } from '../Provider/styles';
+import Button from '../../components/Button';
 import { ContainerStar, StarSolid, StarEmpty } from './styles';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+
+function createService() {
+  swal(
+    <div>
+      <legend>Regístrate</legend>
+      <form>
+        <fieldset>
+          <label htmlFor="initLoc">Inicio</label>
+          <input type="text" name="initLoc" id="initLoc" placeholder="Inicio" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="finalLoc">Destino</label>
+          <input
+            type="text"
+            name="finalLoc"
+            id="finalLoc"
+            placeholder="Destino"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="date">Fecha de servicio</label>
+          <input 
+            type="date" 
+            name="date" 
+            id="date" />
+        </fieldset>
+      </form>
+    </div>,
+    {
+      buttons: {
+        cancel: 'Cancelar',
+        catch: {
+          text: 'Crear servicio',
+          value: 'catch',
+        },
+      },
+    }
+  ).then((value) => {
+    switch (value) {
+      case 'catch':
+        swal('Felicitaciones', 'Se envío al conductor tu solicitud', 'success');
+        break;
+
+      default:
+        swal('Has cancelado la creación del servicio');
+    }
+  });
+}
 
 function Stars({ services }) {
   let count = '';
-  const rating = services.map((service) => count += service.rating) / services.length;
+  const rating =
+    services.map((service) => (count += service.rating)) / services.length;
 
   return (
     <section>
@@ -42,13 +93,17 @@ function Client({ tows }) {
                 <Stars services={serviceIDs} />
               </ContainerStar>
               <ContainerElement>
-                {`${serviceIDs.length} servicio${serviceIDs.length === 1 ? '' : 's'}`}
-                </ContainerElement>
+                {`${serviceIDs.length} servicio${
+                  serviceIDs.length === 1 ? '' : 's'
+                }`}
+              </ContainerElement>
               <ContainerElement>
-                <Photo
-                  src={supplierID.photo}
-                  alt={supplierID.name}
-                ></Photo>
+                <Photo src={supplierID.photo} alt={supplierID.name}></Photo>
+              </ContainerElement>
+              <ContainerElement>
+                <Button color="primary" onClick={createService}>
+                  Pedir Grúa
+                </Button>
               </ContainerElement>
             </ContainerList>
           );
