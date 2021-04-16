378c6e9055b26eaabf409ac209ed2ec06317455e
Carlos <carlos_arce93@hotmail.com>
2021-04-16 11:04:59 -0500

    implement query when fetching services

--- src/components/Provider/index.js
+++ src/components/Provider/index.js
@@ -3,11 +3,47 @@ import { es } from 'date-fns/locale';
 import { Photo, ContainerList, ContainerElement, SectionList } from './styles';
 import Button from '../../components/Button';
 import Swal from 'sweetalert2';
+import { getServices, deleteError } from '../../store/servicesReducer';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import swal from 'sweetalert'
+import { useHistory } from "react-router-dom";
 
-function Provider({ services }) {
+function Provider() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const { loading, errorServices, services,  userFront } = useSelector(
+    ({ servicesReducer, usersReducer }) => ({
+    loading: servicesReducer.loading,
+    errorServices: servicesReducer.errorServices,
+    services: servicesReducer.services,
+    userFront: usersReducer.userFront,
+  }));
+
+  useEffect(() => {
+    if(userFront.towIDs) {
+      dispatch(getServices(`towID=${userFront.towIDs[0]}`));
+    }
+  }, [userFront.email]);
+
+  if(loading) return <p>loading...</p>
+  if (errorServices) {
+    localStorage.removeItem('token');
+    history.push('/login');
+    swal({
+      title: 'Algo salió mal!',
+      text:
+        'Por favor, ingresa de nuevo a la aplicación con tu usuario y contraseña.',
+      icon: 'error',
+    });
+
+    dispatch(deleteError());
+  }
+
   return (
     <SectionList>
-      {!!services &&
+      {!!services && !!userFront &&
         services.length > 0 &&
         services.map(({ _id, initLoc, finalLoc, date, bikeID }) => {
           const dateArr = date.split('-');
--- src/pages/ListTow/index.js
+++ src/pages/ListTow/index.js
@@ -1,50 +1,16 @@
-import swal from 'sweetalert';
-import { useEffect } from 'react';
-import { useHistory } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
 import { BoxSupplier } from '../ListMotorcycle/styles';
-import { getServices, deleteError } from '../../store/servicesReducer';
 import Provider from '../../components/Provider';
 import Button from '../../components/Button';
 import NavBar from '../../components/NavBar';
 
 function ListTow() {
-  const dispatch = useDispatch();
-  const { loading, services, userID, errorServices, userFront } = useSelector(
-    ({ servicesReducer, usersReducer }) => ({
-    loading: servicesReducer.loading,
-    services: servicesReducer.services,
-    userID: servicesReducer.userID,
-    errorServices: servicesReducer.errorServices,
-    userFront: usersReducer.userFront,
-  }));
-
-  useEffect(() => {
-    dispatch(getServices(`towID=${userFront.towIDs[0]}`));
-  }, []);
-
-  let history = useHistory();
-  
-  if(loading) return <p>loading...</p>
-  if (errorServices) {
-    localStorage.removeItem('token');
-    history.push('/login');
-    swal({
-      title: 'Algo salió mal!',
-      text:
-        'Por favor, ingresa de nuevo a la aplicación con tu usuario y contraseña.',
-      icon: 'error',
-    });
-
-    dispatch(deleteError());
-  }
 
   return (
     <section>
-      <NavBar userID={userID}/>
+      <NavBar/>
       <BoxSupplier>
         <Button color="primary">Ha ganado XX.XXX COP</Button>
-        <Provider services={services} />
+        <Provider/>
         <Button color="success">Servicio en proceso</Button>
       </BoxSupplier>
     </section>
