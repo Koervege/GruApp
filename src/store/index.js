6cedfd107a644fe03860ef2827622541fd4ecf64
Carlos <carlos_arce93@hotmail.com>
2021-04-16 15:01:59 -0500

    FIxed email confirmation logic

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,11 @@ import thunk from 'redux-thunk';
 import { towsReducer } from './towsReducer';
 import { servicesReducer } from './servicesReducer';
 import { usersReducer } from './usersReducer';
-import { loginReducer } from './loginReducer';
 
 const rootReducer = combineReducers({
   towsReducer,
   servicesReducer,
   usersReducer,
-  loginReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
