7757263793affd637c76fc901c701705904aa5dc
Alexis Franco <alexisfrancolopez@gmail.com>
2021-04-18 21:04:29 -0500

    Fixed margin of the page

--- src/pages/ListMotorcycle/styles.js
+++ src/pages/ListMotorcycle/styles.js
@@ -5,10 +5,12 @@ export const BoxSupplier = styled.div`
   align-items: center;
   flex-direction: column;
   padding: 5px 0;
+  margin: 10px 0;
 `;
 
 export const LineDivider = styled.div`
   width: 90%;
   border-top: 3px solid;
   margin: 0 auto;
+  margin: 15px 0;
 `;
