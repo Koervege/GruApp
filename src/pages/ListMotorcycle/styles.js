f672a0db1a39e1296b23718b2e4e9f681ae3df8e
Felipe Soto Correa <pipesoto13@gmail.com>
2021-04-19 19:55:41 -0500

    Styled lists

--- src/pages/ListMotorcycle/styles.js
+++ src/pages/ListMotorcycle/styles.js
@@ -4,8 +4,9 @@ export const BoxSupplier = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  padding: 5px 0;
-  margin: 10px 0;
+  padding: 15px 0;
+  background: linear-gradient(150deg, #c9f1ff 0%, #fefefe 100%);
+  min-height: 100vh;
 `;
 
 export const LineDivider = styled.div`
