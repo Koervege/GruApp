70d9d8136208af2b59bd73dce93ffa5a180c4d62
pipesoto13 <51277503+pipesoto13@users.noreply.github.com>
2021-04-29 14:43:46 -0500

    Add interactive map, get info from geocoding API, and fix styles in landing page
    * Add Map to Landing page
    
    * Style map
    
    * responsive
    
    * Add styles and connect to reversegeolocation API
    
    * Add  .env variables
    
    * Add marker
    
    * Change
    
    * Change styles

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -5,7 +5,7 @@ import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-sv
 import NavBar from '../../components/NavBar';
 import { Background } from '../../components/Background'
 import {
-  LandingMain,
+  StyledH2,
   LandingMap,
   LandingP,
   LineDivider,
@@ -16,6 +16,7 @@ import {
   IconContentContainer,
 } from './styles';
 
+import Map from "../../components/Map";
 class Landing extends React.Component {
 
   render() {
@@ -23,9 +24,8 @@ class Landing extends React.Component {
       <section className="landingContainer">
         <NavBar />
         <Background>
-          <LandingMain>
-            <LandingMap src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d43071.609450190146!2d-75.5761599635311!3d6.260759558366631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d6.2579499!2d-75.55469169999999!4m3!3m2!1d6.2871843!2d-75.58804549999999!5e0!3m2!1sen!2sco!4v1615145272798!5m2!1sen!2sco" />
-          </LandingMain>
+          <Map/>
+          <StyledH2>Conoce más de los servicios que tenemos para ofrecerte</StyledH2>
           <ContentSection>
             <ContentContainer>
               <IconContentContainer>
